f832542ee7e9ee93ae844248a5deb4924e340e82
Robert McCabe <rmccabe1023@gmail.com>
2024-04-19 09:52:18 -0400

    update file

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -13,41 +13,10 @@
       event.preventDefault();
 
       let thisForm = this;
-
       let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-
-      if (!action) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
-
       let formData = new FormData(thisForm);
 
-      if (recaptcha) {
-        if (typeof grecaptcha !== "undefined") {
-          grecaptcha.ready(function () {
-            try {
-              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
-                .then(token => {
-                  formData.set('recaptcha-response', token);
-                  php_email_form_submit(thisForm, action, formData);
-
-                })
-            } catch (error) {
-              displayError(thisForm, error);
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-
-      }
+      php_email_form_submit(thisForm, action, formData);
     });
   });
 
@@ -55,22 +24,19 @@
     fetch(action, {
       method: 'POST',
       body: formData,
-      headers: { 'X-Requested-With': 'XMLHttpRequest' }
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      }
     })
-      .then(response => {
-        if (response.ok) {
-          return response.text();
-        } else {
-          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
-        }
-      })
+      .then(response => response.json()) // Parse response as JSON
       .then(data => {
         thisForm.querySelector('.loading').classList.remove('d-block');
-        if (data.trim() == 'OK') {
-          thisForm.querySelector('.sent-message').classList.add('d-block');
-          thisForm.reset();
+        if (data.ok) {
+          // Handle success response
+          window.location.href = data.next; // Redirect to the next page
         } else {
-          throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
+          // Handle error response
+          throw new Error(data.error || 'Unknown error occurred'); // Throw error with error message from server, if available
         }
       })
       .catch((error) => {
@@ -83,7 +49,4 @@
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
-
-
-
 })();
