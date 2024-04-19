6ab62af2086ff2cba00bd56f58e10df4e35fea8c
Robert McCabe <rmccabe1023@gmail.com>
2024-04-19 10:13:14 -0400

    update file

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,4 +1,3 @@
-
 (function () {
   "use strict";
 
@@ -10,9 +9,39 @@
 
       let thisForm = this;
       let action = thisForm.getAttribute('action');
+      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+
+      if (!action) {
+        displayError(thisForm, 'The form action property is not set!');
+        return;
+      }
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
       let formData = new FormData(thisForm);
 
-      php_email_form_submit(thisForm, action, formData);
+      if (recaptcha) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
+            try {
+              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
+                .then(token => {
+                  formData.set('recaptcha-response', token);
+                  php_email_form_submit(thisForm, action, formData);
+
+                })
+            } catch (error) {
+              displayError(thisForm, error);
+            }
+          });
+        } else {
+          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+        }
+      } else {
+        php_email_form_submit(thisForm, action, formData);
+
+      }
     });
   });
 
@@ -20,19 +49,22 @@
     fetch(action, {
       method: 'POST',
       body: formData,
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest'
-      }
+      headers: { 'X-Requested-With': 'XMLHttpRequest' }
     })
-      .then(response => response.json()) // Parse response as JSON
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        }
+      })
       .then(data => {
         thisForm.querySelector('.loading').classList.remove('d-block');
-        if (data.ok) {
-          // Handle success response
-          window.location.href = data.next; // Redirect to the next page
+        if (data.trim() == 'OK') {
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
         } else {
-          // Handle error response
-          throw new Error(data.error || 'Unknown error occurred'); // Throw error with error message from server, if available
+          throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
         }
       })
       .catch((error) => {
@@ -45,4 +77,8 @@
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
+
 })();
+
+
+
